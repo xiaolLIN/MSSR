4ecbae71ed91454bf99ca00b83cdca055f0eb544
xiaolLIN <153204720+xiaolLIN@users.noreply.github.com>
2024-08-28 16:14:15 +0800

    Add files via upload

--- run_MSSR.sh
+++ run_MSSR.sh
@@ -3,36 +3,36 @@
 result_file='result.txt'
 
 python run_model.py --gpu_id=1 --model=MSSR --dataset="yelp" \
-           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.12 --aaplmd=9 --sim='dot' \
+           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.12 --sim='dot' \
 		       --n_layers=4 --n_heads=8  \
-           --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
+           --aaplmd=9 --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
            --ip_mode='gating'  \
            --train_batch_size=1024 --pooling_mode='mean' --config_files="configs/yelp_ac.yaml" \
            --attribute_hidden_size=[256] --ada_fuse=1 --fusion_type=gate --result_file=${result_file};
 
 
 python run_model.py --gpu_id=1 --model=MSSR --dataset="Amazon_Toys_and_Games" \
-           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.08 --aaplmd=3 --sim='dot' \
+           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.08 --sim='dot' \
            --n_layers=3 --n_heads=4  \
-           --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
+           --aaplmd=3 --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
            --ip_mode='gating' \
            --train_batch_size=2048 --pooling_mode='mean' --config_files="configs/Amazon_Toys_and_Games_ac.yaml" \
            --attribute_hidden_size=[128] --ada_fuse=1 --fusion_type=gate --result_file=${result_file};
 
 
 python run_model.py --gpu_id=1 --model=MSSR --dataset="Amazon_Beauty" \
-           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.14 --aaplmd=4 --sim='dot' \
+           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.14 --sim='dot' \
            --n_layers=3 --n_heads=2  \
-           --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
+           --aaplmd=4 --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
            --ip_mode='gating' \
            --train_batch_size=2048 --pooling_mode='mean' --config_files="configs/Amazon_Beauty_ac.yaml" \
            --attribute_hidden_size=[256] --ada_fuse=1 --fusion_type=gate --result_file=${result_file};
 
 
 python run_model.py --gpu_id=1 --model=MSSR --dataset="Amazon_Sports_and_Outdoors"  \
-           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.14 --aaplmd=2 --sim='dot' \
+           --ssl=1 --cl='idropwc' --tau=1 --cllmd=0.14 --sim='dot' \
            --n_layers=2 --n_heads=8  \
-           --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
+           --aaplmd=2 --attribute_predictor='linear' --aap='wi_wc_bce' --aap_gate=1 \
            --ip_mode='gating'  \
            --train_batch_size=1024 --pooling_mode='mean' --config_files="configs/Amazon_Sports_and_Outdoors_ac.yaml" \
            --attribute_hidden_size=[256] --ada_fuse=1 --fusion_type=gate --result_file=${result_file}
--- run_model.py
+++ run_model.py
@@ -8,50 +8,29 @@ if __name__ == '__main__':
     begin = time.time()
     parameter_dict = {
         'neg_sampling': None,
-        # 'gpu_id':3,
-        # 'attribute_predictor':'not',
-        # 'attribute_hidden_size':"[256]",
-        # 'fusion_type':'gate',
-        # 'seed':212,
-        # 'n_layers':4,
-        # 'n_heads':1
     }
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', '-m', type=str, default='MSSR', help='name of models')
     parser.add_argument('--dataset', '-d', type=str, default='Amazon_Beauty', help='name of datasets')
     parser.add_argument('--config_files', type=str, default='configs/Amazon_Beauty.yaml', help='config files')
-
-    parser.add_argument('--ada_fuse', type=int, default=1)   # adaptive learnable att fusion weight
-
     parser.add_argument('--hidden_size', type=int, default=256)
-    parser.add_argument('--item_predictor', type=int, default=2)
-    parser.add_argument('--ip_gate_mode', type=str, default='moe')
-    parser.add_argument('--gate_drop', type=int, default=1)
-
-    parser.add_argument('--clayer_num', type=int, default=1)
+    parser.add_argument('--pooling_mode', type=str, default='mean')
+    
     parser.add_argument('--n_layers', type=int)
     parser.add_argument('--n_heads', type=int)
 
+    parser.add_argument('--ada_fuse', type=int, default=1)   # adaptive learnable att fusion weight
+    parser.add_argument('--ip_mode', type=str, default='gating')
+
+    parser.add_argument('--aaplmd', type=int, default=5)
     parser.add_argument('--aap', type=str, default='wi_wc_bce')
     parser.add_argument('--app_gate', type=int, default=1)
-    parser.add_argument('--lamdas', type=int, default=5)
-
-    parser.add_argument('--logit_num', type=int, default=2)
-    parser.add_argument('--pooling_mode', type=str, default='sum')
-
-    parser.add_argument('--seqmc', type=str, default='all')
-    parser.add_argument('--cdmc', type=str, default='all')
-    parser.add_argument('--sc', type=int, default=1)
 
     parser.add_argument('--ssl', type=int, default=1)
     parser.add_argument('--cl', type=str, default='idropwc')
     parser.add_argument('--tau', type=float, default=1)
     parser.add_argument('--cllmd', type=float, default=0.1)
     parser.add_argument('--sim', type=str, default='dot')
-
-    parser.add_argument('--pred', type=str, default='dot')
-
-    parser.add_argument('--mask_ratio', type=float, default=0.2)
 
     parser.add_argument('--result_file', type=str)
     args, _ = parser.parse_known_args()
